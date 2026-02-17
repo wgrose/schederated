ddc553513cff84e877be9bdeb81b562757aefd31
William Grose <williamgrose@gmail.com>
2026-02-17 06:23:28 -0800

    Add show password toggle

---
+++ flutter_schederated/lib/probe_auth.dart
@@ -0,0 +1,9 @@
+
+import 'package:flutter/material.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+
+void main() {
+  SignInScreen(
+    showPasswordVisibilityToggle: true,
+  );
+}
